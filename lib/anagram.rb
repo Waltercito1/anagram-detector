61541ad6b34f79acafb73055c8b76372d0837797
Walter Aab <aabwalter@yahoo.com>
2020-11-03 20:49:03 -0600

    commit tree 8b7f2f1e5bc36a4c92cbc25fc786c4a8e08e2890

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    attr_accessor :word
+
+    def match(array_of_words)
+        matches  = []
+        array_of_words.each do |element|
+
+            if element.split("").sort == @word.split("").sort
+                matches << element
+
+            end
+        end
+        matches
+    end
+
+end
